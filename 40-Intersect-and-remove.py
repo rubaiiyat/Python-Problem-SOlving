46eb369f6ab34b65ccf939c859e0466c00ef1e40
rubaiiyat <abirr1516@gmail.com>
2024-09-11 23:02:34 +0600

    Intersect and Remove

---
+++ 40-Intersect-and-remove.py
@@ -0,0 +1,5 @@
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("Intersection is", first_set & second_set)
+print("First Set after removing common element", first_set - second_set)
