fbe1b1dc248f0ff75ece40009d0b577ec1db27cd
rubaiiyat <abirr1516@gmail.com>
2024-09-24 23:46:53 +0600

    Beecrowd 1115

---
+++ 102-Quadrant.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        a, b = list(map(int, input().split()))
+
+        if a == 0 and b == 0:
+            break
+        elif a > 0 and b > 0:
+            print("primeiro")
+        elif a > 0 and b < 0:
+            print("quarto")
+        elif a < 0 and b < 0:
+            print("terceiro")
+        elif a < 0 and b > 0:
+            print("segundo")
+
+    except EOFError:
+        break
