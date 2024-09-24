b2c5cecf3ec4d4333d5584762c0c305cc95dc0e0
rubaiiyat <abirr1516@gmail.com>
2024-09-24 20:51:37 +0600

    Beecrowd 1564

---
+++ 96-Brazil_World_Cup.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n = int(input())
+
+        if n == 0:
+            print("vai ter copa!")
+
+        else:
+            print("vai ter duas!")
+    except EOFError:
+        break
